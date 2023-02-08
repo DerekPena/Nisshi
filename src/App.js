4c31bac0df93d549e9bfcd75f772846ce6796029
DerekPena <derekgpena@gmail.com>
2023-02-08 10:46:24 -0500

    Edited the styling of the page.

--- src/App.js
+++ src/App.js
@@ -10,16 +10,25 @@ function App() {
     setData(val.target.value)
     console.warn(val.target.value)
   }
+
   return (
     <div className="App">
+      <div className="App-login">
+        <button style={{color:"blue"}}>Login</button>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>NISSHI</h1>
+      </header>
+      <div className="App-content">
+        <h7>Please enter in your text below and it will display at the bottom of the screen.</h7>
+        <p></p>
         <input type="text" onChange={getData} 
         style={{height:"300px", width:"500px", top:"100px"}}/>
         <h3>{data}</h3>
-        <button style={{position:"10px"}}>Login</button>
-      </header>
+      </div>
+      <div className="App-content">
+        <h3>{data}</h3>
+      </div>
     </div>
   );
 }
