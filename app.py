bdc0a902d63f86cbbc61a30759e1352e52ac4ae1
Azuredc <dariancheung18@hotmail.com>
2023-02-19 16:06:51 -0500

    Reformmatted App.js; Added saving feature

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, jsonify
 from src.backend.settings import app
 from pymongo import MongoClient
 from flask_cors import CORS, cross_origin
 import json, datetime
 
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={'/*':{'origins': 'http://localhost:3000'}})
+app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
+
+journal_entry = None
 
 @app.route('/')
 def home():
@@ -19,6 +21,13 @@ def test():
         'project':'uf23-senior-project',
         'date':x
     }
+
+@app.route('/post', methods=["GET", "POST"])
+def post():
+    global journal_entry
+    if request.method == "POST":
+        journal_entry = request.get_json()
+    return journal_entry
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/frontend/App.js
+++ src/frontend/App.js
@@ -4,27 +4,53 @@ import Sidebar from './sidebar';
 
 function App() {
   const [data, setData]=useState(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch('http://localhost:5000/post',{
+       method: 'POST',
+       headers: {'Content-type': 'application/json'},
+       body: JSON.stringify(data)
+    })
+      .then(response => {return response.json()})
+      .catch(error => {console.log("Error: ", error)})
+
+    console.log(`Form submitted, ${data}`);
+  }
+
   function getData(val)
   {
     //setData stores the text entry in a variable, updating as the user types
     setData(val.target.value)
-    console.warn(val.target.value)
   }
 
   return (
     <div className="App">
       <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+
       <header className="App-header">
         <h1>NISSHI</h1> 
         <h3 style={{color:"#ffb400"}}>日誌</h3>
       </header>
+
       <div className="App-content" id="body">
         <h7>Please enter in your text below and it will display at the bottom of the screen.</h7>
         <p></p>
-        <textarea placeholder="Type here..." onChange={getData} 
-        style={{height:"300px", width:"500px", wordWrap:"break-word", wordBreak:"break-all", fontFamily:"serif"}}/>
-        <h3>{data}</h3>
+
+        <form onSubmit = {handleSubmit} method="post" action='http://localhost:5000/post'>
+            <textarea 
+              placeholder="Type here..."
+              id = "journal" 
+              onChange={getData}
+              style={{height:"300px", width:"500px", wordWrap:"break-word", wordBreak:"break-all", fontFamily:"serif"}}
+            />
+            
+            <h3>{data}</h3>
+            <button type = 'submit'>Save entry</button>
+        </form>
       </div>
+
     </div>
   );
 }
