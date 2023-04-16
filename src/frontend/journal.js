787e28c5f80d7dd9e8a68324aee3ecb53808a02b
DerekPena <derekgpena@gmail.com>
2023-04-16 19:59:14 -0400

    Added basic conjugation detection for ru verbs.

--- src/frontend/journal.js
+++ src/frontend/journal.js
@@ -74,26 +74,95 @@ export default props => {
     function checkVocab() {
         let index = 0;
         let indexKanji = 0;
-        let tempEntry = entry;
-        tempEntry = tempEntry.replaceAll("<strong style=\"color: rgb(255, 180, 0);\">", "");
-        tempEntry = tempEntry.replaceAll("</strong>", "");
+        let settingEntry = entry;
+        let word = "";
+        let wordKanji = "";
+        let conjugated = false;
+        settingEntry = settingEntry.replaceAll("<strong style=\"color: rgb(255, 180, 0);\">", "");
+        settingEntry = settingEntry.replaceAll("</strong>", "");
+        let runningEntry = settingEntry;
+
+        //Known Bug: Only checks for words once when conjugated and once unconjugated.
         for(let key of vocabData.keys())
         {
-            console.log(vocabData[key]);
-            let word = vocabData[key].tango;
-            let wordKanji = vocabData[key].kanji;
-            index = tempEntry.indexOf(word);
-            indexKanji = tempEntry.indexOf(wordKanji);
-            if(index != -1)
-            {
-                tempEntry = (tempEntry.slice(0, index) + "<strong style=\"color: rgb(255, 180, 0);\">" + tempEntry.slice(index, index + word.length) + "</strong>" + tempEntry.slice(index + word.length));
-            }
-            if(indexKanji != -1)
-            {
-                tempEntry = (tempEntry.slice(0, indexKanji) + "<strong style=\"color: rgb(255, 180, 0);\">" + tempEntry.slice(indexKanji, indexKanji + wordKanji.length) + "</strong>" + tempEntry.slice(indexKanji + wordKanji.length));
-            } 
+            let runningEntry = settingEntry;
+                if(vocabData[key].type == "verb")
+                {
+                    if(vocabData[key].type2 == "ru")
+                    {
+                        word = vocabData[key].tango;
+                        wordKanji = vocabData[key].kanji;
+                        word = word.replace("る", "");
+                        wordKanji = wordKanji.replace("る", "");
+                        index = settingEntry.indexOf(word);
+                        indexKanji = settingEntry.indexOf(wordKanji);
+                        if(index != -1)
+                        {
+                            if(settingEntry.substring(index + word.length, index + word.length + 2) == "ます")
+                            {
+                                word = word + "ます";
+                                conjugated = true;
+                            }
+                            else if(settingEntry.substring(index + word.length, index + word.length + 3) == "ません")
+                            {
+                                word = word + "ません";
+                                conjugated = true;
+                            }
+
+                            if(conjugated)
+                            {
+                                settingEntry = (settingEntry.slice(0, index) + "<strong style=\"color: rgb(255, 180, 0);\">" + settingEntry.slice(index, index + word.length) + "</strong>" + settingEntry.slice(index + word.length));
+                                //runningEntry = runningEntry.substring(index);
+                                conjugated = false;
+                            }
+                            
+                        }
+                        else if(indexKanji != -1)
+                        {
+                            if(settingEntry.substring(index + wordKanji.length, index + wordKanji.length + 2) == "ます")
+                            {
+                                wordKanji = wordKanji + "ます";
+                                conjugated = true;
+                            }
+                            else if(settingEntry.substring(index + wordKanji.length, index + wordKanji.length + 3) == "ません")
+                            {
+                                wordKanji = wordKanji + "ません";
+                                conjugated = true;
+                            }
+                            if(conjugated)
+                            {
+                                settingEntry = (settingEntry.slice(0, indexKanji) + "<strong style=\"color: rgb(255, 180, 0);\">" + settingEntry.slice(indexKanji, indexKanji + wordKanji.length) + "</strong>" + settingEntry.slice(indexKanji + wordKanji.length));
+                                //runningEntry = runningEntry.substring(index);
+                                conjugated = false;
+                            }
+                        } 
+                    }
+                    else if(vocabData[key].type == "u")
+                    {
+                        //U-verbs
+                    }
+                    else{
+                        // Need to account for irregular verbs
+                    }
+                }
+
+                // Default case (Nouns, Infinitives, etc)
+                word = vocabData[key].tango;
+                wordKanji = vocabData[key].kanji;
+                index = settingEntry.indexOf(word);
+                indexKanji = settingEntry.indexOf(wordKanji);
+                if(index != -1)
+                {
+                    settingEntry = (settingEntry.slice(0, index) + "<strong style=\"color: rgb(255, 180, 0);\">" + settingEntry.slice(index, index + word.length) + "</strong>" + settingEntry.slice(index + word.length));
+                    //runningEntry = runningEntry.substring(index);
+                }
+                else if(indexKanji != -1)
+                {
+                    settingEntry = (settingEntry.slice(0, indexKanji) + "<strong style=\"color: rgb(255, 180, 0);\">" + settingEntry.slice(indexKanji, indexKanji + wordKanji.length) + "</strong>" + settingEntry.slice(indexKanji + wordKanji.length));
+                    //runningEntry = runningEntry.substring(index);
+                } 
         } 
-        setEntry(tempEntry);
+        setEntry(settingEntry);
     } 
 
     //If the edit button was clicked, Prefills the journal page with the that journal's title and entry
