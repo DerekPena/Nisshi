e05f85643c250f909eeaeb57feeb170f70e22baf
tiff wu <82773100+tiff-wu@users.noreply.github.com>
2023-04-16 22:35:31 -0400

    Updated entry listings front-end.

--- src/frontend/entry.js
+++ src/frontend/entry.js
@@ -26,14 +26,34 @@ export default props => {
     }
 
     //Loops through list of Journals and creates individual panels with each Journal entry
-    const journals=journalData.map( journal => {        
-        return <div key={journal.id} className="container">
-            <p className="journalTitle">{journal.title}</p>
-            <p className="date">{journal.date}</p>
-            <button className="edit" onClick={() => handleEdit(journal.title, journal.entry, journal.journal_id)}>Edit</button>
+    const journals=journalData.map( 
+        journal => { return <div key={journal.id} className="col">
+            <div class="card" id ="entry-card">
+
+                <div class="card-body">
+                    <div className="card-title">{journal.title}</div>
+                    <div className="card-subtitle mb-2 text-muted">{journal.date}</div>
+
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-6">
+                                <button className="btn" onClick={() => handleEdit(journal.title, journal.entry, journal.journal_id)}>EDIT</button>
+                            </div>
+
+                            <div class="col-6">
+                                <button className="btn" onClick={() => handleEdit(journal.title, journal.entry, journal.journal_id)}>DELETE</button>
+                            </div>
+                        </div>
+                    </div>
+
+                
+                </div>
+            </div>
+            
+            
             {/* <p className="entry" dangerouslySetInnerHTML={{ __html: journal.entry }}></p> */}
             {/* <p className="entry">{journal.entry}</p> */}
-            </div>  
+        </div>  
     })
 
     //When users click on the edit button, they are sent to the journal page to edit their journal entry
@@ -52,11 +72,23 @@ export default props => {
 
     return(
         <form className="entry-container">
-            <h2 className="entry-title">{name}'s Journal Entries</h2>
+            <div class="container">
+                <div class="row" id="main-container">
+                    <div class="col-10">
+                        <h2 className="entry-title">{name}'s Journal Entries</h2>
+                    </div>
+                    <div class="col-2">
+                        <button className="btn" onClick={() => handleNew()}>NEW ENTRY</button>
+                    </div>
+                    
+                    <div class="row row-cols-1 row-cols-md-3 gy-4 g-4">
+                            {journals}
+                    </div>
+                </div>
 
-            <button className="new" onClick={() => handleNew()}>New</button>
+
+            </div>
             
-            {journals}
         </form>
     )
 }
